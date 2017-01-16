b0e0c692b83fcb3620acb29d3345e6e8a0d80ad3
sindunuragarp <sindunuragarp@gmail.com>
2017-01-16 12:48:23 +0100

    Tidied up sebastiaan's code

--- feature_extraction.m
+++ feature_extraction.m
@@ -1,13 +1,14 @@
-function [ dataset ] = feature_extraction( nist_data, im_size )
-    % remove invalid observations
-    a = nist_data*im_box([],0);
-    % add a bounding box to the images to make it square.
-    a = a*im_box([],0,1);
-    % resample the images.
-    method = 'bicubic';% nearest, bilinear and bicubic.
-    a = a*im_resize([],[im_size, im_size], method);
-    % dataset: suitable for handling
-    dataset = prdataset(a, getlabels(a));
-    dataset.prior = ones(10,1)/10; % uniform prior,
-end
+function [dataset] = feature_extraction(im_data)
 
+    % add a bounding box to the images to make it square
+    a = im_data*im_box([],0,1);
+    
+    % resample the images (nearest / bilinear / bicubic)
+    im_size = 10;
+    method = 'bicubic';
+    a = a*im_resize([],[im_size, im_size], method);
+    
+    % reconstruct prdataset
+    dataset = prdataset(a, getlabels(a));
+    dataset.prior = ones(10,1)/10; % uniform prior
+end
--- train.m
+++ train.m
@@ -1,6 +1,6 @@
-function [ w ] = train( train_data , classifier)
-%TRAIN returns the trained classifier. Parameterized for testing and
-%debugging only, should be delivered non-parameterized.
+function [w] = train(train_data, classifier)
+%TRAIN returns the trained classifier.
+%Parameterized for testing and debugging only.
+%Should be delivered non-parameterized.
     w = classifier(train_data);
 end
-
